839c2febd6683eb6ef70a79e4ffbb7461164d1d3
crepindev <nathan.j.crepin@gmail.com>
2024-03-14 02:53:59 +1100

    fixed errors with HTMLNode and LeafNode unit tests as well as zero-nest case for ParentNode. Still working on nested case for ParentNode...

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,7 +1,7 @@
 class HTMLNode:
-    def __init__(self, value=None, tag=None, children=None, props=None):
-        self.value = value
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
+        self.value = value
         self.children = children
         self.props = props
 
@@ -9,22 +9,49 @@ class HTMLNode:
         raise NotImplementedError()
 
     def props_to_html(self):
-        if self.props == {"href": "https://www.google.com", "target": "_blank"}:
-            return ' href="https://www.google.com" target="_blank"'
+        if self.props is None:
+            return ""
+        else:
+            props_str = ""
+            for key in self.props:
+                key_value = self.props[key]
+                props_str += f' {key}="{key_value}"'
+            return props_str
         
     def __repr__(self):
-        print(f"value={self.value}, tag={self.tag}, children={self.children}, props={self.props}")
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag=None, children=None, props=None):
-        super().__init__()
-        self.value = value
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
         if self.value is None:
-            raise ValueError()
+            raise ValueError("no value provided")
         elif self.tag is None:
             return self.value
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+        if self.tag is None:
+            raise ValueError("no tag provided")
+        elif self.children is None:
+            raise ValueError("no children provided")
+    
+    def to_html(self):
+        parent_tag_open = f"<{self.tag}>"
+        parent_tag_close = f"</{self.tag}>"
+        child_str = ""
+        for child in self.children:
+            if child.children is None:
+                child_str += f"{child.to_html()}"             
+            else:
+                child.to_html()
+        print(f"DEBUG >>> {parent_tag_open}{child_str}{parent_tag_close}")
+        return f"{parent_tag_open}{child_str}{parent_tag_close}"
+    
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,25 +1,74 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHMTLNode(unittest.TestCase):
-    def test_props_to_html(self):
+    def test1_props_to_html(self):
         props1 = {"href": "https://www.google.com"}
-        props2 = {"href": "https://www.google.com", "target" : "_blank"}
         node1 = HTMLNode(props = props1)
+        self.assertEqual(node1.props_to_html(), ' href="https://www.google.com"')
+        
+    def test2_props_to_html(self):
+        props2 = {"href": "https://www.google.com", "target" : "_blank"}
         node2 = HTMLNode(props = props2)
-        case1 = (node1.props_to_html() == " href = https://www.google.com")
-        case2 = (node2.props_to_html() ==  ' href=https://www.google.com target="_blank"')
-        return case1 and case2
+        self.assertEqual(node2.props_to_html(), ' href="https://www.google.com" target="_blank"')
+
 
 class TestLeafNode(unittest.TestCase):
-    def test_to_html(self):
-        node1 = LeafNode(value="test message",tag="p",props={"href": "https://www.google.com"})
-        node2 = LeafNode(value="fortnite AA",tag="a")
-        case1 = (node1.to_html() == '<p href="https://www.google.com">test message</p>')
-        case2 = (node2.to_html() == '<a>fortnite AA</a>')
-        return case1 and case2
+    def test1_to_html(self):
+        node1 = LeafNode(tag="p", value="test message",props={"href": "https://www.google.com"})
+        self.assertEqual(node1.to_html(), '<p href="https://www.google.com">test message</p>')
+
+    def test2_to_html(self):
+        node2 = LeafNode(tag="a", value="fortnite AA")
+        self.assertEqual(node2.to_html(), '<a>fortnite AA</a>')
+
+
+class TestParentNode(unittest.TestCase):
+    def test1_to_html(self):
+        node1 = ParentNode(tag="p",
+            children=[
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ]
+        )
+        self.assertEqual(node1.to_html(), 
+            "<p>"\
+                "<b>Bold text</b>"\
+                "Normal text"\
+                "<i>italic text</i>"\
+                "Normal text"\
+            "</p>")
+    
+    def test2_to_html(self):
+        node2 = ParentNode(tag="p",
+            children=[
+                ParentNode(tag="p",
+                    children=[
+                    LeafNode("b", "Bold text"),
+                    LeafNode("b", "Bold text")
+                    ]
+                ),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ]
+        )
+        self.assertEqual(node2.to_html(), 
+            "<p>"\
+                "<p>"\
+                    "<b>Bold text</b>"\
+                    "<b>Bold text</b>"\
+                "</p>"\
+                "Normal text"\
+                "<i>italic text</i>"\
+                "Normal text"\
+            "</p>"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()

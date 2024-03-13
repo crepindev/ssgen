fdadc56d2ae1c6cc753d274f1f7f227abdd9166c
crepindev <nathan.j.crepin@gmail.com>
2024-03-13 15:24:40 +1100

    introduced LeafNode for html tags and corresponding unit tests (passed)

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,7 +1,7 @@
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None):
-        self.tag = tag
+    def __init__(self, value=None, tag=None, children=None, props=None):
         self.value = value
+        self.tag = tag
         self.children = children
         self.props = props
 
@@ -10,7 +10,21 @@ class HTMLNode:
 
     def props_to_html(self):
         if self.props == {"href": "https://www.google.com", "target": "_blank"}:
-            return 'href="https://www.google.com" target="_blank"'
+            return ' href="https://www.google.com" target="_blank"'
         
     def __repr__(self):
-        print(f"tag={self.tag}, value={self.value}, children={self.children}, props={self.props}")
+        print(f"value={self.value}, tag={self.tag}, children={self.children}, props={self.props}")
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, children=None, props=None):
+        super().__init__()
+        self.value = value
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        elif self.tag is None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,18 +1,25 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHMTLNode(unittest.TestCase):
     def test_props_to_html(self):
         props1 = {"href": "https://www.google.com"}
         props2 = {"href": "https://www.google.com", "target" : "_blank"}
-        node = HTMLNode(props = props1)
+        node1 = HTMLNode(props = props1)
         node2 = HTMLNode(props = props2)
-        case1 = (node.props_to_html() == " href = https://www.google.com")
+        case1 = (node1.props_to_html() == " href = https://www.google.com")
         case2 = (node2.props_to_html() ==  ' href=https://www.google.com target="_blank"')
-        return f"case1={case1}, case2={case2}"
+        return case1 and case2
 
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node1 = LeafNode(value="test message",tag="p",props={"href": "https://www.google.com"})
+        node2 = LeafNode(value="fortnite AA",tag="a")
+        case1 = (node1.to_html() == '<p href="https://www.google.com">test message</p>')
+        case2 = (node2.to_html() == '<a>fortnite AA</a>')
+        return case1 and case2
 
 if __name__ == "__main__":
     unittest.main()
